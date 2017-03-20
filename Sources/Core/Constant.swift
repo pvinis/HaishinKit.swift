3ed8302f35d00144820d35b36961a9710ace0694
Pavlos Vinieratos <pvinis@gmail.com>
2017-03-20 17:30:09 +0100

    bla

--- Sources/Core/Constant.swift
+++ Sources/Core/Constant.swift
@@ -1,3 +1,12 @@
+
+class Logger {
+  func error(_ message: String) {
+    print(message)
+  }
+}
+
+var logger = Logger()
+
 public enum CMSampleBufferType: String {
     case video = "video"
     case audio = "audio"
