24764a0eadfc5b5a2fda32c147ec3e731d43c62b
Pavlos Vinieratos <pvinis@gmail.com>
2017-08-01 15:41:38 +0200

    personalize

--- Examples/iOS/Preference.swift
+++ Examples/iOS/Preference.swift
@@ -4,5 +4,5 @@ struct Preference {
     static var defaultInstance:Preference = Preference()
     
     var uri:String? = "rtmp://stream-staging-eu.mycujoo.tv/live"
-    var streamName:String? = "ecf34b4aea4f41b99ef4933c775baf8c"
+    var streamName:String? = "9acb05f05e434f47bf4b8f8fbe8a4fe0"
 }
